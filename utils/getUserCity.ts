10f4499fff6087427e52f5a4bc054d12ff750add
finedineholdthewine <bryonlmon@icloud.com>
2025-04-11 19:04:37 -0700

    Update getUserCity.ts
    starter code

--- utils/getUserCity.ts
+++ utils/getUserCity.ts
@@ -1,1 +1,9 @@
-
+export async function getUserCity(ip: string): Promise<string> {
+  try {
+    const res = await fetch(`https://ipapi.co/${ip}/json/`);
+    const data = await res.json();
+    return data.city || 'your area';
+  } catch (error) {
+    return 'your area';
+  }
+}
