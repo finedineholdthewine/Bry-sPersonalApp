d2ec84e3b6926851fc1afb78bcd0a83952192a4b
finedineholdthewine <bryonlmon@icloud.com>
2025-04-11 22:44:34 -0700

    Update getCryptoPrice.ts
    Resaved code

--- utils/getCryptoPrice.ts
+++ utils/getCryptoPrice.ts
@@ -1,5 +1,5 @@
 export async function getCryptoPrice(coin: string = 'bitcoin') {
-  const res = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`);
-  const data = await res.json();
-  return data[coin]?.usd || 'Unavailable';
+  const res = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`);
+  const data = await res.json();
+  return data[coin]?.usd || 'Unavailable';
 }
